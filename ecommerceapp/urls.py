ebaed45af535a00342640aa72f0c8537a7382848
Yash Singhal <ysinghal555@gmail.com>
2023-10-22 19:54:11 +0530

    Initial commit

---
+++ ecommerceapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('handlerequest/', views.handlerequest, name="handleRequest"),
+]
